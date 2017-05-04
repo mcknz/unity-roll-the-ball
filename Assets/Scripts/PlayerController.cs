d5aefe3ffc25a5ccfef20f426c2d292b50fbf81a
Jeff McKenzie <mail@mcknz.com>
2017-05-04 17:57:21 -0400

    added PlayerController class methods to update player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // called just before performing any physics calculations
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(
+            moveHorizontal,
+            0.0f,
+            moveVertical);
+
+        rb.AddForce(movement);
+
+    }
 }
