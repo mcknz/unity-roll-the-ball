3acadf3e584691058ea2733197bf9b4be83baef1
Jeff McKenzie <mail@mcknz.com>
2017-05-16 14:59:10 -0400

    added camera controller to scene, to allow camera to track player with an offset

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    // LateUpdate every frame,  runs after all items have been processed in update
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
